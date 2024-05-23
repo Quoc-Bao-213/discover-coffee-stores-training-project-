380bc320fed8ac680bf3e7c7d70f6606f847515a
BaoTa99 <bao.ta@s3corp.com.vn>
2024-05-23 17:27:44 +0700

    Feat(components): Build card component and add coffee stores on Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import Banner from "@/components/banner.client";
-import Image from "next/image";
+import Card from "@/components/card.server";
 
 export default function Home() {
+  const coffeeStoreId = "dark-horse-coffee";
   return (
     <div className="mb-56">
       <main className="mx-auto mt-10 max-w-6xl px-4">
         <Banner />
+
+        <div className="mt-20">
+          <h2 className="mt-8 pb-8 text-4xl font-bold text-white">
+            Toronto Stores
+          </h2>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
+            {coffeeStores.map((coffeeStore: CoffeeStoreType, idx: number) => (
+              <Card
+                key={`${coffeeStore.name}-${coffeeStore.id}`}
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${coffeeStore.id}?id=${idx}`}
+              />
+            ))}
+          </div>
+        </div>
       </main>
     </div>
   );
